518beb306d937b354969d32a2ac817c1cfd5311f
IvisLuka404 <152911709+IvisLuka404@users.noreply.github.com>
2023-12-04 18:27:51 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://IvisLuka404/final'
+  base: 'ivisluka404.github.io/final/'
 })
